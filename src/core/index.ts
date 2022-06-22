70acabce934348c91468ae83cedcd05545af2642
mihalichpalich <pavlovtfspb@gmail.com>
2022-06-22 13:34:38 +0300

    added router class

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export {default as Block} from './Block';
 export {default as registerComponent} from './registerComponent';
 export {default as renderDOM} from './renderDOM';
 export {default as HTTPTransport} from './HTTPTransport';
+export {default as Router} from './Router';
