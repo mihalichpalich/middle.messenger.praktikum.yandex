e1bb1d9a6d3d8cf6c9550eb6b25dff4b972d849c
mihalichpalich <pavlovtfspb@gmail.com>
2022-07-16 18:38:37 +0300

    added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,67 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    compress: true,
+    port: 4000,
+    open: true,
+    hot: true
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'project-name.bundle.js'
+    filename: 'bundle.js'
   },
   resolve: {
+    alias: {
+      'handlebars': 'handlebars/dist/handlebars.js',
+    },
+    fallback: {
+      'fs': false
+    },
     extensions: ['.ts', '.js', '.json']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin(),
+    new Dotenv()
+  ],
   module: {
     rules: [
       {
-        test: /\.ts?$/,
-        loader: 'ts-loader',
-        exclude: ['/node_modules/'],
-      }
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+              transpileOnly: true
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(?:svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 };
